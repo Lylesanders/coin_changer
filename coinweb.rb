93ead43f339a06c092b6048612cff2a6f61f6f44
lyle Sanders <lylesanders66@yahoo.com>
2016-09-25 15:16:16 -0400

    passing name and coins parameter

--- coinweb.rb
+++ coinweb.rb
@@ -2,11 +2,24 @@ require 'sinatra'	#start web service
 require_relative 'coinfunction.rb'
 
 
-get '/' do
+get '/' do 
 	erb :get_name
+
 end
 
 post '/users_name' do 
-  name = params[:user_name]
-  "Hello #{name}"
+	name = params[:user_name]  # user_name matches get_name <input type ="text" name ="user_name">
+	redirect '/age?users_name=' + name # users_name matches get_name <form metopd "post" action"/users_name>
+end
+
+get '/age' do 
+	name = params[:users_name]
+	erb :get_change , :locals =>{:name => name}
+end
+ 
+
+post '/change' do 
+ 	change = params[:age]
+ 	name = params[:retrieved_name]
+ 	"Hello #{name} you entered #{change}"
 end
